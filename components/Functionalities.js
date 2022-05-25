204a7cdc9dcb9b3166d501b6bf0db4ea64569116
ujjwal_kirti <kirti.chargers@gmail.com>
2022-05-25 13:07:25 +0530

    25/05/2022

--- components/Functionalities.js
+++ components/Functionalities.js
@@ -4,13 +4,14 @@ import Card from "./Card";
 
 function Functionalities() {
   const [functionalities, setFunctionalities] = React.useState([]);
+  const [loading, setLoading] = React.useState(true);
   React.useEffect(() => {
     axios
       .get("/api/getfunctionalities")
       .then(function (response) {
         // handle success
         setFunctionalities(response.data);
-        
+        setLoading(false);
       })
       .catch(function (error) {
         // handle error
@@ -19,10 +20,13 @@ function Functionalities() {
   }, []);
 
   return (
-    <div className="w-4/6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 bg-white mt-2">
-      {functionalities.map((functionality) => {
-        return <Card option={functionality} />;
-      })}
+    <div>
+      <div className="w-4/6 mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 bg-white mt-2">
+        {functionalities.map((functionality) => {
+          return <Card option={functionality} />;
+        })}
+      </div>
+      {loading && <img src="spinner.jpg" className="h-72 rounded-full mx-auto"/>}
     </div>
   );
 }
